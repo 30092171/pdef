ac18d7a1b8497952f5107ed43d8e669c8a8a5956
Peter Lefaivre <noodlegamesdev@gmail.com>
2020-02-04 14:40:04 -0700

    "no u"

--- src/runner/TestClass.java
+++ src/runner/TestClass.java
@@ -3,6 +3,6 @@ package runner;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("yeet");
-		System.out.println("boi");
+		System.out.println("boreri");
 }
 }
