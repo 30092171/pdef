471946a8d2d4a35f9c198bae5d2297f3ee9cf9c6
richa <richa@10.13.76.132>
2020-02-07 13:21:56 -0700

    Test

--- src/runner/GoodJob.java
+++ src/runner/GoodJob.java
@@ -3,7 +3,7 @@ package runner;
 public class GoodJob {
 	public static void main(String[] args) {
 	
-	System.out.print("YOur doin A GREAT JOB. kep it up");
+	System.out.print("yYOur doin A GREAT JOB. kep it up");
 	
 	}
 
