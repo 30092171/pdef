85ce58f9ebed671881ddb7d2929ff3da8ed50d8d
30092171 <nick.zhao@ucalgary.ca>
2020-02-07 13:40:37 -0700

    add main class skeleton

---
+++ src/pdef/Main.java
@@ -0,0 +1,18 @@
+package pdef;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		PlayerPlanet p = new PlayerPlanet();
+		while(true) {
+			System.out.println("player stats here");
+			String input = sc.nextLine();
+			//if(p.isGameOver){break;}
+		}
+	}
+
+}
