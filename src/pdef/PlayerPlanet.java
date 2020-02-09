ad1217dfaf5814165cce613fbf6acd491c3cc2e6
Nick Zhao <46830311+30092171@users.noreply.github.com>
2020-02-08 19:07:32 -0700

    Main, SpawnHandler, Projectile updates (#8) (#9)
    * Create SpawnHandler Class
    
    * Clear repo
    
    * Add Planet Class
    
    * Changed Class Name
    
    * Clear Repo
    
    * Rough PlayerPlanet; could possibly create constructor to manual adjust
    
    * temporary text input/output depending on game state
    
    * Improvements to Alex's Main function and new SpawnHandler
    
    * Added some comments and organized the main function a little better
    
    Co-authored-by: Peter Lefaivre <noodlegamesdev@gmail.com>
    Co-authored-by: tranalex <55296947+tranalex@users.noreply.github.com>

--- src/pdef/PlayerPlanet.java
+++ src/pdef/PlayerPlanet.java
@@ -1,5 +1,53 @@
 package pdef;
 
 public class PlayerPlanet {
+	private int numLives;
+	private int score;
+	private double planetRadius;
+	private int xCoordinate;
+	private int yCoordinate;
+	
+	public PlayerPlanet() {
+		numLives = 3;
+		score = 0;
+		planetRadius = 10; 
+		xCoordinate = 0;
+		yCoordinate = 0;
+	}
+	
+	public void lostLife() {
+		numLives -= 1;
+	}
+	
+	public void addLife() {
+		numLives += 1;
+	}
+	
+	public boolean isGameOver() {
+		if (numLives <= 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void addScore(int givenScore) {
+		score += givenScore;
+	}
+	
+	public int getXPos() {
+		return xCoordinate;
+	}
+	
+	public int getYPos() {
+		return yCoordinate;
+	}
+	
+	public int getLives() {
+		return numLives;
+	}
 
 }
--- src/pdef/Projectile.java
+++ src/pdef/Projectile.java
@@ -1,5 +1,7 @@
 package pdef;
 
 public class Projectile {
+	String name = "A";
+	int distance = 100;
 
 }
