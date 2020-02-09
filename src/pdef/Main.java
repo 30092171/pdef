c5c950f861fe02ffc7d8140b9876465a408be066
Nick Zhao <46830311+30092171@users.noreply.github.com>
2020-02-08 19:01:48 -0700

    Main, SpawnHandler, Projectile updates (#8)
    * Create SpawnHandler Class
    
    * Clear repo
    
    * Add Planet Class
    
    * Changed Class Name
    
    * Clear Repo
    
    * Rough PlayerPlanet; could possibly create constructor to manual adjust
    
    * temporary text input/output depending on game state
    
    * Improvements to Alex's Main function and new SpawnHandler
    
    * Added some comments and organized the main function a little better
    
    Co-authored-by: Peter Lefaivre <noodlegamesdev@gmail.com>
    Co-authored-by: tranalex <55296947+tranalex@users.noreply.github.com>

--- src/pdef/Main.java
+++ src/pdef/Main.java
@@ -1,18 +1,80 @@
 package pdef;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
+	
+	static Scanner sc = new Scanner(System.in);
+	static PlayerPlanet p = new PlayerPlanet();
+	static ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		PlayerPlanet p = new PlayerPlanet();
-		while(true) {
-			System.out.println("player stats here");
+		//Initial projectile list
+		projectiles.add(SpawnHandler.spawnProjectile());
+		projectiles.add(SpawnHandler.spawnProjectile());
+		projectiles.add(SpawnHandler.spawnProjectile());
+		
+		//Main game loop. continues until player runs out of lives
+		while(p.getLives() > 0) {
+			System.out.println("----------------------------------");
+			System.out.println("You have " + p.getLives() + " lives remaining");
+			System.out.println("Current Score: " + p.getScore());
+			
+			for(int projectile = 0; projectile < projectiles.size(); projectile++) {
+				System.out.println("Projectile " + projectiles.get(projectile).name + " is " + projectiles.get(projectile).distance + " units away.");
+			}
+			
+			System.out.print("Enter projectile to shoot:");
 			String input = sc.nextLine();
-			//if(p.isGameOver){break;}
+			System.out.println("----------------------------------");
+			
+			//Checks hit or miss
+			int projectilesBefore = projectiles.size(); //Used to compare if projectile is destroyed later
+			for(int projectile = 0; projectile < projectiles.size(); projectile++) {
+				if (input.contentEquals(projectiles.get(projectile).name)) {
+					System.out.println("Projectile " + projectiles.get(projectile).name + " Destroyed!");
+					projectiles.remove(projectile);
+					p.addScore(100);
+					break;
+				}
+			}
+			//If the number of projectiles hasn't changed, a miss has occured.
+			if (projectiles.size() == projectilesBefore) {
+				System.out.println("Miss!");
+			}
+			
+			//Decreases projectile distance after each turn and removes projectile/life if distance is 0.
+			for (int i = 0 ; i < projectiles.size(); i++) {
+				int distance = projectiles.get(i).distance;
+				projectiles.get(i).distance = (int)( distance - ( 7 + (Math.random()*10)));
+				 
+				if (projectiles.get(i).distance <= 0){
+					System.out.println("Impact Detected, -1 Life!");
+					p.lostLife();
+					projectiles.remove(i);
+				}
+			}
+			
+			//Projectile spawning after the rest of the turn's game logic has occured. 
+			//If there are no projectiles left, then more will always be spawned
+			if(projectiles.size() < 1) {
+				projectiles.add(SpawnHandler.spawnProjectile());
+				projectiles.add(SpawnHandler.spawnProjectile());
+			}
+			if(projectiles.size() < 4) {
+				if (Math.random() > 0.5) {
+					projectiles.add(SpawnHandler.spawnProjectile());
+					projectiles.add(SpawnHandler.spawnProjectile());
+				}
+			}
 		}
+		
+		//Game over messages, execute if the main game loop is broken by running out of lives
+		System.out.println("----------------------------------");
+		System.out.println("Game Over!");
+		System.out.println("Final Score: " + p.getScore());
 	}
 
 }
--- src/pdef/SpawnHandler.java
+++ src/pdef/SpawnHandler.java
@@ -1,5 +1,24 @@
 package pdef;
+import java.lang.Math;
 
 public class SpawnHandler {
-
+	static private String[] namesList = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 		
+										"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
+										"U", "V", "W", "X", "Y", "Z"};
+	static int nameIndex = 0;
+	
+	public static Projectile spawnProjectile() {
+		Projectile newProjectile = new Projectile();
+		newProjectile.name = namesList[nameIndex];
+		newProjectile.distance = (int) (10 + Math.random() * 100);
+		if ((nameIndex + 1) > (namesList.length - 1)) {
+			nameIndex = 0;
+		}
+		else {
+			nameIndex = nameIndex + 1;
+		}
+		return  newProjectile;
+	}
+	
 }
+
