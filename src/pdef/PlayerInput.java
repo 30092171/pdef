4f2cff31fe7843f05eb3a1c56c35211b0077e673
richa <richa@DESKTOP-RUDV303.hitronhub.home>
2020-02-09 00:28:55 -0700

    Now checks if user inputed a valid projectile.

--- src/pdef/PlayerInput.java
+++ src/pdef/PlayerInput.java
@@ -1,5 +1,7 @@
 package pdef;
 
+import java.util.ArrayList;
+
 public class PlayerInput {
 	
 	private PlayerPlanet p;
@@ -9,16 +11,32 @@ public class PlayerInput {
 	}
 	
 	public void command(String input) {
+		input = input.toUpperCase();
 		if (input.length() == 1) {
-			char a = input.toUpperCase().charAt(0);
-			//for (char pr : p.projectileArray) {
-				//if (pr == a) {
-				//pr.tryDamage();
-				//}
-			//}
+			String projUser = input;
+			for (int count = 0 ; count < Main.projectiles.size() ; count++) {
+				String projName = Main.projectiles.get(count).name;
+				if (projUser.equals(projName)) {
+				//projUser.tryDamage();
+				}
+				//if projectile named not in play
+				else {
+					
+				}
+			}
 		}
-		//next: regular commands and actions
+
+		//to reset/start new game
+		if(input.equals("RESET")) {
+		
+		}
+		
+		//for invalid input
+		else {
+			
+		}
+	
 	}
-	
-	
+
 }
+
