2376a817a5f2f0f7fb587714f9af916875b142ca
30092171 <nick.zhao@ucalgary.ca>
2020-02-26 10:48:21 -0700

    added starter code for javafx application

---
+++ src/fxapplication/Main.java
@@ -0,0 +1,14 @@
+package fxapplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.show();
+	}
+}
