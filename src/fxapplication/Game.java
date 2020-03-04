b617ba1c62192108bf2e039b8c25035ce7ebadeb
Peter Lefaivre <noodlegamesdev@gmail.com>
2020-03-04 14:38:20 -0700

    Started implementation of the HUD elements. Proper alignment, etc.

--- src/fxapplication/Game.java
+++ src/fxapplication/Game.java
@@ -2,16 +2,29 @@ package fxapplication;
 
 import javafx.application.Application;
 import javafx.geometry.Dimension2D;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.geometry.*;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 public class Game extends Application {
 	
-	private static Canvas CANVAS;
-	private static Dimension2D SIZE;
+	private static Dimension2D SIZE = new Dimension2D(720, 720);
+	private static Canvas CANVAS = new Canvas(SIZE.getWidth(), SIZE.getHeight());
+	private static BorderPane root = new BorderPane();
+	private static Scene scene = new Scene(root);
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -27,26 +40,63 @@ public class Game extends Application {
 	
 	private static void initWindow(Stage mainStage) {
 		mainStage.setTitle("Planet Defenders");
-		SIZE = new Dimension2D(800, 800);
-		CANVAS = new Canvas(SIZE.getWidth(), SIZE.getHeight());
-		Group root = new Group();
-		Scene scene = new Scene(root);
 		root.getChildren().add(CANVAS);
+		root.setPrefSize(720, 720);
 		mainStage.setScene(scene);
+	}
+	
+	public static void drawTopHUD() {
+		
+		//Define Boxes
+		HBox livesBox = new HBox();
+			livesBox.setPadding(new Insets(-68, 0, 0, 20));
+			livesBox.setAlignment(Pos.TOP_LEFT);
+			livesBox.setSpacing(15);
+			root.setLeft(livesBox);
+		VBox scoreBox = new VBox();
+			scoreBox.setPadding(new Insets(20, 0, 0, 0));
+			scoreBox.setAlignment(Pos.TOP_CENTER);
+			root.setTop(scoreBox);
+		HBox controlsBox = new HBox();
+			controlsBox.setPadding(new Insets(-68, 20, 0, 0));
+			controlsBox.setAlignment(Pos.TOP_RIGHT);
+			root.setRight(controlsBox);
+		
+		//Populate Lives HBox
+		int LIFESIZE = 35;
+		Rectangle life1 = new Rectangle(LIFESIZE,LIFESIZE);
+			livesBox.getChildren().add(life1);
+		Rectangle life2 = new Rectangle(LIFESIZE,LIFESIZE);
+			livesBox.getChildren().add(life2);
+		Rectangle life3 = new Rectangle(LIFESIZE,LIFESIZE);
+			livesBox.getChildren().add(life3);
+			
+		//Populate Score VBox
+		Label scoreText = new Label("SCORE");
+			scoreText.setFont(new Font("Arial", 14));
+			scoreBox.getChildren().add(scoreText);
+		Label scoreValue = new Label("0");
+			scoreValue.setFont(new Font("Arial", 48));
+			scoreBox.getChildren().add(scoreValue);
+		
+		//Populate Controls HBox
+		Button pauseButton = new Button("Pause");
+			controlsBox.getChildren().add(pauseButton);
 	}
 	
 	public void start(Stage mainStage) throws Exception {
 		initWindow(mainStage);
-		testDraw();
+		drawTopHUD();
+		drawPlanet();
 		mainStage.show();
 	}
 	
-	public static void testDraw() {
+	
+	public static void drawPlanet() {
 		GraphicsContext gc = CANVAS.getGraphicsContext2D();
 		
-		int radius = 100;
+		int radius = 65;
 		//**topleft x,y, width height
 		gc.fillOval(SIZE.getWidth()/2 - radius, SIZE.getHeight()/2 - radius, radius * 2, radius * 2);
-		gc.strokeText("SCORE", SIZE.getWidth() / 2 - 20, 20);
 	}
 }
