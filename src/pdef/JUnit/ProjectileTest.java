b08ed359e9864aa0f70a0e56f87d70e788869e84
richa <richa@DESKTOP-RUDV303.hitronhub.home>
2020-04-04 21:27:57 -0600

    Added testToString (w javaDoc) + cleaned up testGetCircle.

--- src/pdef/JUnit/ProjectileTest.java
+++ src/pdef/JUnit/ProjectileTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Circle;
 import pdef.DefaultProjectile;
@@ -105,11 +106,10 @@ class ProjectileTest {
 	@Test
 	public void testGetCircle(){
 		Point2D origin = new Point2D(400,300);
-		Circle testCircle = new Circle(300,300,25);
 		Projectile Projectile1 = new DefaultProjectile("Projectile C", new PolarCoord (100,Math.PI,origin),new Circle(25));
-		assertEquals(testCircle.getCenterX(), Projectile1.getCircle().getCenterX(),0.01);
-		assertEquals(testCircle.getCenterY(), Projectile1.getCircle().getCenterY(),0.01);
-		assertEquals(testCircle.getRadius(), Projectile1.getCircle().getRadius(),0.01);
+		assertEquals(300, Projectile1.getCircle().getCenterX(),0.01);
+		assertEquals(300, Projectile1.getCircle().getCenterY(),0.01);
+		assertEquals(25, Projectile1.getCircle().getRadius(),0.01);
 	}
 	
 	/**
@@ -131,6 +131,16 @@ class ProjectileTest {
 		Projectile Projectile1 = new DefaultProjectile("Projectile C", new PolarCoord(69, 4.20, origin),new Circle(100));
 		Projectile1.setCircleRadius(75);
 		assertEquals(75, Projectile1.getCircleRadius(),0.001);
+	}
+	
+	/**
+	 * Tests toString.
+	 */
+	@Test
+	void testToString() {
+		Point2D origin = new Point2D(400,300);
+		Projectile Projectile1 = new DefaultProjectile("Projectile A", new PolarCoord (100, 10, origin));
+		assertEquals("Projectile: [class pdef.PolarCoord]: distance: 100.0, radians: 10.0", Projectile1.toString());
 	}
 }
 
